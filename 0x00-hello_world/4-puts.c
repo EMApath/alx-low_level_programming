388a14e6e0fdb44d04167140e9fc42a88748c857
Eman Bashir <eman_bashir@outlook.com>
2023-12-15 02:01:30 +0300

    a c programme that prints a script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - prints a string
+ * Return: Always 0 (success)
+ */
+int main(){
+
+puts("\"Programming is like building a multilingual puzzle\n");
+
+return 0;
+}
