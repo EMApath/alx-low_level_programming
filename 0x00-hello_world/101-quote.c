16b22ffaf7e810365dca709977e97cfb871af99c
Eman Bashir <eman_bashir@outlook.com>
2023-12-18 00:29:06 +0300

    printin intel syntax

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print a string
+ *
+ * Return: 1 success
+ */
+int main()
+{
+char str[i] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+putchar('i',++);
+putchar('\n');
+return (1);
+}
