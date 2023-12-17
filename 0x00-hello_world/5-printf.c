ae53b79fe8ed86a7274a78c9452e50e09daa5633
Eman Bashir <eman_bashir@outlook.com>
2023-12-17 21:52:33 +0300

    print a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - use printf to print string
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
