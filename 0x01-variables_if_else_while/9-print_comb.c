2ca91f96051d10df946325fe370a1e1f8430734d
Eman Bashir <eman_bashir@outlook.com>
2024-01-19 01:58:52 +0300

    print combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int ctr;
+
+	for (ctr = 48; ctr <= 57; ctr++)
+	{
+		putchar(ctr);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
