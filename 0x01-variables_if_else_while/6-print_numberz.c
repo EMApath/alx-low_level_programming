2ec5c7fcd5083c639c4bc2e85319d2ad65773ae7
Eman Bashir <eman_bashir@outlook.com>
2024-01-19 01:08:03 +0300

    print nums without a char type

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <=57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
