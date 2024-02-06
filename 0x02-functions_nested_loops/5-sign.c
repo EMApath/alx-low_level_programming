6ba5000850bff5ee1a508f8c7408eee3aff53f75
Eman Bashir <eman_bashir@outlook.com>
2024-02-06 19:31:01 +0300

    prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,24 @@
 /**
  * print_sign - prints the sign of a number.
  * @pnum: number to print the sign of
- * Return: 0 if successfull
+ * Return: 1 if +ve number, -1 for -ve num or zero otherwise
  */
 int print_sign(int pnum)
 {
-	if (pnum > '0')
+	if (pnum > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else
-		if (pnum == 0)
+		if (pnum < 0)
 	{
-		_putchar('0');
-		return (0);
-	}
-	else
-		if (pnum < '0')
-	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
-	return (0);
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
